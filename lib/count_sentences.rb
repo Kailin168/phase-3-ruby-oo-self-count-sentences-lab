8ec306f8b9a70d44180d175b0726cb0176c3b5b6
Kai L <kailin8591@gmail.com>
2022-07-19 21:24:42 -0400

    finished

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
-    
+    result = self.end_with?(".")
+    return result
   end
 
   def question?
-
+    result = self.end_with?("?")
+    return result
   end
 
   def exclamation?
-
+    result = self.end_with?("!")
+    return result
   end
 
-  def count_sentences
+  # def count_sentences
+  #   count = 0
+  #   prev = ""
+  #   self.split('').each do |character| 
+  #     if character == "." && prev != character
+  #       count += 1
+  #     elsif character == "!" && prev != character
+  #       count += 1
+  #     elsif character == "?" && prev != character
+  #       count += 1
+  #     end
+  #     prev=character
+  #   end
+  #   count
+  # end
 
+  def count_sentences
+    self.split(/[\.\!\?]+/).count
   end
 end
